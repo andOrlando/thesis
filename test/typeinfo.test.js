4496f39adfcd62d0f7d466032995111a79070fb4
andOrlando <bennettgillig@gmail.com>
2025-09-30 23:10:31 -0400

    adds typeinfo tests and fixes typeinfos
    - adds tests for objects and primitives
    - fixes bugs with typeinfo for objects not working

---
+++ test/typeinfo.test.js
@@ -0,0 +1,38 @@
+import { describe, it } from "node:test"
+import assert from "node:assert"
+import { compute_typeinfo, PrimitiveTI, ObjectTI } from "../instrument/instrument.ts"
+
+
+describe("type to typeinfo", () => { 
+  it("should handle primitives", () => {
+    assert.equal(compute_typeinfo(3).toString(), new PrimitiveTI(3).toString())
+  })
+
+  it("should handle functions", () => {
+    
+
+  })
+
+  it("should handle objects", () => {
+    assert.equal("{a:number,b:string}", compute_typeinfo({ a: 1, b: "hi" }).toString())
+  })
+
+  it("should handle nested objects", () => {
+    assert.equal("{a:{b:number}}", compute_typeinfo({ a: {b: 1} }).toString())
+  })
+
+  it("should handle top-level cyclical objects", () => {
+    let o = {}
+    o.o = o
+    assert.equal("<ref *0>{o:<Circular *0>}", compute_typeinfo(o).toString())
+  })
+
+  it("should handle non-top-level cyclical objects", () => {
+    let o = {}
+    o.O = {}
+    o.O.o = o.O
+    assert.equal("{O:<ref *0>{o:<Circular *0>}}", compute_typeinfo(o).toString())
+  })
+})
+
+describe("trace hashing")
